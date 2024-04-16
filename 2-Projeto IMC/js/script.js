7018bf821bc2ce697c279338c0fb174dc63383d4
Lucas <simoesdacostalucas@gmail.com>
2024-04-16 13:58:27 -0300

    created an alert-error.js file and an object literal AlertError

--- 2-Projeto IMC/js/script.js
+++ 2-Projeto IMC/js/script.js
@@ -1,4 +1,5 @@
 import {Modal} from './modal.js'
+import {AlertError} from './alert-error.js'
 //variaveis
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
@@ -9,11 +10,23 @@ form.onsubmit = event => {
     const weight = inputWeight.value
     const height = inputHeight.value
 
+    const showAlertError = notANumber(weight)  || notANumber(height)
+
+    if (showAlertError){
+        AlertError.open()
+        return;
+    }
+    AlertError.close()
+
     const result = IMC(weight, height)
     const message =  `Seu IMC é de ${result}`
 
     Modal.message.innerText = message
     Modal.open()
+}
+
+function notANumber(value){
+    return isNaN(value) || value == ""
 }
 
 function IMC(weight, height) {
